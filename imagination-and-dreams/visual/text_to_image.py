deb3bbffe241956c235b1c0966fa42b035018362
LaVoy <brentenlavoy@gmail.com>
2023-01-30 22:32:28 -0700

    Adjustments for 8GB VRAM GPU and multiple image generation.

--- imagination-and-dreams/visual/text_to_image.py
+++ imagination-and-dreams/visual/text_to_image.py
@@ -1,16 +1,22 @@
 import torch
-from diffusers import StableDiffusionPipeline
+from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
 
 model_id = "stabilityai/stable-diffusion-2-1"
 
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+# Define the denoising scheuler
+pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+# Move pipe to GPU
 pipe = pipe.to("cuda")
 
 # For GPUs with low VRAM
-# pipe.enable_attention_slicing()
+pipe.enable_attention_slicing()
 
-prompt = "electric sheep"
-image = pipe(prompt).images[0]
-    
-image.save("an_androids_dream_slice.png")
+prompt = "An android dreaming of electric sheep."
+
+num_images = 8
+
+for i in range(num_images):
+    image = pipe(prompt, height=768, width=768).images[0]
+    image.save(str(i)+".png")
 
