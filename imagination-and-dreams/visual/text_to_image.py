bbeb769d9d30e6e3f824e0671226d8d94ba4b5f5
LaVoy <brentenlavoy@gmail.com>
2023-02-21 20:16:31 -0700

    Adds better naming for saved images and notes on landscapes.

--- imagination-and-dreams/visual/text_to_image.py
+++ imagination-and-dreams/visual/text_to_image.py
@@ -1,5 +1,6 @@
 import torch
 from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
+from datetime import datetime
 
 model_id = "stabilityai/stable-diffusion-2-1"
 
@@ -12,11 +13,19 @@ pipe = pipe.to("cuda")
 # For GPUs with low VRAM
 pipe.enable_attention_slicing()
 
-prompt = "An android dreaming of electric sheep."
+# Landscape types:
+# Desert, Plain, Taiga, 
+# Tundra, Wetland, Mountain, 
+# Mountain range, Cliff, 
+# Coast, Littoral zone, Glacier, 
+# Polar regions of Earth, Shrubland, Forest, 
+# Rainforest, Woodland, Jungle, 
+# Moors, Steppe, Valley.
+prompt = "Flowers in a desert"
 
 num_images = 8
 
 for i in range(num_images):
     image = pipe(prompt, height=768, width=768).images[0]
-    image.save(str(i)+".png")
+    image.save(str(datetime.now()).replace(" ", "_")+".png")
 
